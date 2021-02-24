f704e78de058b2d3184576a97265058736f93e70
Sara Khurana <khuranasara18@gmail.com>
2021-02-25 00:58:18 +0530

    [#3][Sara]-Update tests

--- src/components/posts/postView.test.js
+++ src/components/posts/postView.test.js
@@ -2,6 +2,15 @@ import { render } from "@testing-library/react";
 import { StateProvider } from "../../context/Context";
 import PostView from "./PostView";
 
+jest.mock("../../containers/post/CreatePost", () => () => {
+  return (
+    <>
+      <label>Enter Post</label>
+      <input data-testid="test-create-post" />
+    </>
+  );
+});
+
 describe("PostView", () => {
   it("should render", () => {
     const { asFragment } = render(
@@ -9,6 +18,18 @@ describe("PostView", () => {
         <PostView />
       </StateProvider>
     );
+
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("should give option to create post", () => {
+    const { getByTestId } = render(
+      <StateProvider>
+        <PostView />
+      </StateProvider>
+    );
+    const element = getByTestId("test-create-post");
+
+    expect(element).toBeInTheDocument();
   });
 });
