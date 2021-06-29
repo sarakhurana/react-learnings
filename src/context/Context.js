59487d45f2602ee68ff59e453c084bff414645eb
Sara Khurana <khuranasara18@gmail.com>
2021-06-29 23:15:57 +0530

    [Sara]- delete counter

--- src/context/Context.js
+++ src/context/Context.js
@@ -2,7 +2,6 @@ import React, { useReducer } from "react";
 import rootReducer from "../store/index";
 
 const initialState = {
-  counter: { counter: 0 },
   post: { posts: [] },
 };
 export const Context = React.createContext(initialState);
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import postReducer from "./postReducer";
-import counterReducer from "./counterReducer";
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   post: postReducer,
 });
 export default rootReducer;
